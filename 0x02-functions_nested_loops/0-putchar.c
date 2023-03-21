1f7d86bb67a1bdbfa39df51133f460bae9e4226f
Nigus16 <nigustselassie@gmail.com>
2023-03-21 04:12:54 -0700

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,13 @@
 #include "main.h"
 /**
+ * main - Entry point
+ *Description: "prints _putchar to stdout"
+ *Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');#include "main.h"
+/**
  * main - prints _putchar, fllowd by a new linees
  * Return: Alwys 0 (Success)
  */
@@ -15,4 +23,6 @@ int main(void)
 	}
 	_putchar('\n');
 	return (0);
+}
+
 }
